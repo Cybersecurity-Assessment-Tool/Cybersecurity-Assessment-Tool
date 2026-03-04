1a48b9b20846715bd76b5b1f4e152876656539ad
Cora Patterson <145165542+gottaloveturtles@users.noreply.github.com>
2026-03-04 15:08:23 -0500

    Create scripts folder and test_email.py

---
+++ backend/scripts/test_email.py
@@ -0,0 +1,26 @@
+from api.utils.send_mail import send_mail
+
+def run(*args):
+    """
+    Usage:
+        python manage.py runscript send_email
+        python manage.py runscript send_email --script-args test@example.com
+    """
+
+    # Get recipient from script args (optional)
+    #recipient = args[0] if args else "onellamoitra@gmail.com"
+
+    subject = "SMTP Email from Django RunScript"
+    message = """
+    Hello,
+
+    This email was sent using Django runscript via SMTP.
+
+    Regards,
+    Django App
+    """
+
+    send_mail("onellamoitra@gmail.com", subject, message)
+
+    # or, you can short circuit it like this:
+    # send_mail("onellamoitra@gmail.com", "simple subject", "simple message")
