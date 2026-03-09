6507a82c681acb97549d1cf2d20e86d1193e0fbe
mr245 <shane@lambodesigns.com>
2026-03-09 01:20:59 -0400

    Fixing debug_toolbar issue

--- cybersecurity_assessment_tool/config/settings.py
+++ cybersecurity_assessment_tool/config/settings.py
@@ -75,6 +75,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'debug_toolbar',
     'accounts',
     'api',
     'rest_framework',
@@ -89,6 +90,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
+    
 ]
 
 ROOT_URLCONF = 'config.urls'
