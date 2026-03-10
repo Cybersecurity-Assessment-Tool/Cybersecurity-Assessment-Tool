344d8b3af339353405ba1762df52635712792911
Binnyboy <ibinstock@outlook.com>
2026-03-10 10:28:18 -0700

    resolved merge conflict

--- cybersecurity_assessment_tool/config/settings.py
+++ cybersecurity_assessment_tool/config/settings.py
@@ -75,7 +75,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'debug_toolbar',
     'django_extensions',
     'accounts',
     'api',
