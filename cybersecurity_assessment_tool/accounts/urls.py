0a7f7fe9f9ff09dbae538b19dbb756aea601e025
onellamoitra <125746020+onella-moitra@users.noreply.github.com>
2026-03-13 18:28:48 -0400

    Adding public registration on accounts url page
    This is so that public registration was also accessible in accounts since it deals with signup

--- cybersecurity_assessment_tool/accounts/urls.py
+++ cybersecurity_assessment_tool/accounts/urls.py
@@ -1,9 +1,15 @@
 # accounts/urls.py
 from django.urls import path
+import api.views as views
+from rest_framework.routers import DefaultRouter
 
 from .views import SignUpView
 
 
 urlpatterns = [
-    path("signup/", SignUpView.as_view(), name="signup"),
+    #path("signup/", SignUpView.as_view(), name="signup"),
+    path('register/', SignUpView.as_view(), name='registration'),
+    path('otp/verify/', views.otp_verify_view, name='otp_verify'),
+    path('otp/send/', views.send_otp_view, name='resend_otp'),
+    path('public-signup/', views.public_registration, name='public_registration'),
 ]
