6feb746cedcda1b275159486268b9a351ed8df52
on545169 <onellamoitra@gmail.com>
2026-03-14 22:07:44 -0400

    Fixing Indent

--- cybersecurity_assessment_tool/accounts/views.py
+++ cybersecurity_assessment_tool/accounts/views.py
@@ -15,9 +15,9 @@ class SignUpView(CreateView):
         return form
 
     class SignUpView(CreateView):
-    form_class = UserCreationForm
-    success_url = reverse_lazy("login")
-    template_name = "registration/registration_dummy.html"
+        form_class = UserCreationForm
+        success_url = reverse_lazy("login")
+        template_name = "registration/registration_dummy.html"
 
     def get_form(self, form_class=None):
         form = super().get_form(form_class)
