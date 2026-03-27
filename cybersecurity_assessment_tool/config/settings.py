420fed6bd38967c737e704c2ff050476352e4b64
Binnyboy <ibinstock@outlook.com>
2026-03-27 07:10:50 -0700

    Reverted my changes back to a working version

--- cybersecurity_assessment_tool/config/settings.py
+++ cybersecurity_assessment_tool/config/settings.py
@@ -211,22 +211,27 @@ INTERNAL_IPS = [
     '127.0.0.1'
 ]
 
-
 LOGIN_REDIRECT_URL = 'login_redirect'
 LOGOUT_REDIRECT_URL = "home"
 
-# Email settings
+# ---------------------------------------------------------------------------
+# Email Configuration
+# ---------------------------------------------------------------------------
 EMAIL_BACKEND_TYPE = os.environ.get('EMAIL_BACKEND_TYPE', 'console')
 
-if EMAIL_BACKEND_TYPE == 'smtp':
+if ENVIRONMENT == 'local':
+    # Prints emails to the terminal during local development
+    EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+else:
+    # Uses Gmail SMTP for integration/staging/production on Heroku
     EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
     EMAIL_HOST = 'smtp.gmail.com'
     EMAIL_PORT = 587
     EMAIL_USE_TLS = True
-    EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
-    EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
-else:
-    EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+    
+    # These must be set in your Heroku Config Vars
+    EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')       # e.g., shanelambert@ucf.edu
+    EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD') # Your 16-character Google App Password 
 
 TESTING = 'test' in sys.argv or 'PYTEST_VERSION' in os.environ
 
