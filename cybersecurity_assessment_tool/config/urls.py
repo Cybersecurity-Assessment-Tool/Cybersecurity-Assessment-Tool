e57d9994868fa93291f681f9acaab15e8f333532
Binnyboy <ibinstock@outlook.com>
2026-03-29 15:21:37 -0700

    got rid of unneeded profile tab

--- cybersecurity_assessment_tool/config/urls.py
+++ cybersecurity_assessment_tool/config/urls.py
@@ -39,9 +39,6 @@ urlpatterns = [
 
     path('tasks/<str:task_id>/', views.check_task_status, name='check_task_status'),
 
-    # path('settings/', views.settings, name='settings'),  # /settings/
-    path('profile/', views.profile, name='profile'),  # /profile/
-
     ## DEBUG
     path('test-email/', views.test_sendgrid, name='test_email'),
 ]
